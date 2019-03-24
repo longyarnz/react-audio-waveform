90afa603b849f01b9864649f33e325e23ac7e039
Olalekan Ayodele <longyarnz@gmail.com>
2019-03-24 14:02:47 +0100

    Create waveform component

--- test/src/AudioPlayer.js
+++ test/src/AudioPlayer.js
@@ -3,6 +3,7 @@ import ReactHowler from 'react-howler';
 import PropTypes from 'prop-types';
 
 import { PlayButton, Progress, Timer } from 'react-soundplayer/components';
+import AudioUI from './AudioUI';
 
 const DEFAULT_DURATION = 456.1495; // have to use this become modifying the audio file breaks 2x speed
 const DEFAULT_MP3 = "https://parse-server-ff.s3.amazonaws.com/ae5992f0f5bb1f259bafa41b3771e3bb_call12565815456dwwwwww795896232www-01b59bd3.mp3";
@@ -130,6 +131,7 @@ class AudioPlayer extends Component {
                         ref={(ref) => (this.player = ref)}
                     />
                 </div>
+                <AudioUI duration={duration} currentTime={currentTime} />
             </div>
         );
     }
---
+++ test/src/AudioUI.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import WaveformData from 'waveform-data';
+import Waveform from 'react-audio-waveform';
+
+export default class AudioUI extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            waveform: null
+        };
+
+        this.fetchAudioFile = this.fetchAudioFile.bind(this);
+    }
+
+    handleClick(e) {
+        console.log('Clicked', e);
+    }
+
+    componentDidMount() {
+        this.fetchAudioFile();
+    }
+
+    fetchAudioFile() {
+        const DEFAULT_MP3 = "https://parse-server-ff.s3.amazonaws.com/ae5992f0f5bb1f259bafa41b3771e3bb_call12565815456dwwwwww795896232www-01b59bd3.mp3";
+        fetch(DEFAULT_MP3)
+            .then(response => response.arrayBuffer())
+            .then(buffer => {
+                const waveform = WaveformData.create(buffer);
+                this.setState({ waveform });
+            });
+    }
+
+    render() {
+        const { waveform } = this.state;
+        const { max = [] } = waveform || {};
+        return max.length === 0 ? (
+            <div>Loading...</div>
+        )
+            : (
+                <Waveform
+                    barWidth={4}
+                    peaks={max}
+                    height={100}
+                    pos={this.props.currentTime}
+                    duration={this.props.duration}
+                    onClick={this.handleClick}
+                    color="#ccc"
+                    progressColor="#f00"
+                />
+            );
+    }
+}
